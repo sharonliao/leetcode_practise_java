8a2b954b9e7f08bd8986704b80efc89a57989939
Xiaoyun Liao <sharon.liaoxy@gmail.com>
2021-06-06 22:06:47 -0400

    add recursive solution of 25

--- src/Linked_List/Reverse_Nodes_in_kGroup_25.java
+++ src/Linked_List/Reverse_Nodes_in_kGroup_25.java
@@ -23,11 +23,10 @@ public class Reverse_Nodes_in_kGroup_25 {
             nextTemp = curr.next;
             curr.next = prev;
             prev = curr;
-            if(count==k){
-                head.next = nextTemp;
-            }
             curr = nextTemp;
         }
+        //s
+        head.next = nextTemp;
         System.out.println("prev:"+prev.val);
 
         Pair<ListNode, ListNode> pair = new Pair<>(prev, head);
@@ -97,6 +96,72 @@ public class Reverse_Nodes_in_kGroup_25 {
         result += curNode.val;
         return result;
     }
+
+
+
+    public ListNode reverseLinkedList(ListNode head, int k) {
+
+        // Reverse k nodes of the given linked list.
+        // This function assumes that the list contains
+        // atleast k nodes.
+        ListNode new_head = null;
+        ListNode ptr = head;
+
+        while (k > 0) {
+
+            // Keep track of the next node to process in the
+            // original list
+            ListNode next_node = ptr.next;
+
+            // Insert the node pointed to by "ptr"
+            // at the beginning of the reversed list
+            ptr.next = new_head;
+            new_head = ptr;
+
+            // Move on to the next node
+            ptr = next_node;
+
+            // Decrement the count of nodes to be reversed by 1
+            k--;
+        }
+
+
+        // Return the head of the reversed list
+        return new_head;
+
+    }
+
+    public ListNode reverseKGroup_recursive(ListNode head, int k) {
+
+        int count = 0;
+        ListNode ptr = head;
+
+        // First, see if there are at least k nodes left in the linked list.
+        while (count < k && ptr != null) {
+            ptr = ptr.next;
+            count++;
+        }
+
+
+        // If we have k nodes, then we reverse them
+        if (count == k) {
+
+            // Reverse the first k nodes of the list and
+            // get the reversed list's head.
+            ListNode reversedHead = this.reverseLinkedList(head, k);
+
+            // Now recurse on the remaining linked list. Since
+            // our recursion returns the head of the overall processed
+            // list, we use that and the "original" head of the "k" nodes
+            // to re-wire the connections.
+            head.next = this.reverseKGroup(ptr, k);
+            return reversedHead;
+        }
+
+        return head;
+    }
+
+
 
     public static void main(String[] args){
         Reverse_Nodes_in_kGroup_25 test = new Reverse_Nodes_in_kGroup_25();
